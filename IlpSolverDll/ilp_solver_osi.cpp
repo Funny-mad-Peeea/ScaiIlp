af6de0c768cb8585f1161ade36104bf19abfbe85
Tobias Brunsch <tobias.brunsch@scai.fraunhofer.de>
2015-12-22 13:46:33 +0100

    Added concrete class ILPSolverOsi that is completely based on the OsiSolverInterface

---
+++ IlpSolverDll/ilp_solver_osi.cpp
@@ -0,0 +1,23 @@
+#include "ilp_solver_osi.hpp"
+
+#include "OsiSolverInterface.hpp"
+
+namespace ILPSolver
+{
+    ILPSolverOsi::ILPSolverOsi(OsiSolverInterface* const p_ilp_solver) : d_ilp_solver(p_ilp_solver) {}
+
+    void ILPSolverOsi::do_solve()
+    {
+        solver()->branchAndBound();
+    }
+
+    const double* ILPSolverOsi::do_get_solution() const
+    {
+        return solver()->getColSolution();
+    }
+
+    double ILPSolverOsi::do_get_objective() const
+    {
+        return solver()->getObjValue();
+    }
+}
---
+++ IlpSolverDll/ilp_solver_osi.hpp
@@ -0,0 +1,28 @@
+#ifndef _ILP_SOLVER_OSI_HPP
+#define _ILP_SOLVER_OSI_HPP
+
+#include "ilp_solver_osi_model.hpp"
+
+class OsiSolverInterface;
+
+namespace ILPSolver
+{
+    // Wrapper class for all ILP solvers that implement the OsiSolverInterface
+    class ILPSolverOsi: public ILPSolverOsiModel
+    {
+    public:
+        ILPSolverOsi(OsiSolverInterface* const p_ilp_solver);
+
+        private:
+            OsiSolverInterface* const d_ilp_solver;
+
+            OsiSolverInterface*         solver              () override         { return d_ilp_solver; }
+            const OsiSolverInterface*   solver              () const override   { return d_ilp_solver; }
+
+            void                        do_solve            () override;
+            const double*               do_get_solution     () const override;
+            double                      do_get_objective    () const override;
+    };
+}
+
+#endif
