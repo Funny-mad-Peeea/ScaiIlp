110c14200849057d563501c3432f517be6265ab0
Tobias Brunsch <tobias.brunsch@scai.fraunhofer.de>
2015-12-22 13:14:40 +0100

    Added DLL import/export macro

---
+++ IlpSolverDll/IlpSolverDll.hpp
@@ -0,0 +1,10 @@
+#ifndef _ILP_SOLVER_DLL_HPP
+#define _ILP_SOLVER_DLL_HPP
+
+#ifdef ILP_SOLVER_DLL_EXPORTS
+    #define ILP_SOLVER_DLL_API __declspec(dllexport) 
+#else
+    #define ILP_SOLVER_DLL_API __declspec(dllimport)
+#endif
+
+#endif
