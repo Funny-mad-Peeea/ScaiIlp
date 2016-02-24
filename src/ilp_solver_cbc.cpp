4a48b55ceff609fd94013ba915c835f3aea0651d
Tobias Brunsch <tobias.brunsch@scai.fraunhofer.de>
2016-02-24 08:59:38 +0100

    ILPSolverCbc: Temporarily deactivated setting number of threads
    Currently, we cannot compile and link Cbc to support multi-threading. If we can, we will uncomment this line again.

--- src/ilp_solver_cbc.cpp
+++ src/ilp_solver_cbc.cpp
@@ -18,7 +18,7 @@ namespace ilp_solver
     void ILPSolverCbc::do_solve()
     {
         d_model = CbcModel(*solver());
-        d_model.setNumberThreads(num_threads());
+//        d_model.setNumberThreads(num_threads());
         d_model.messageHandler()->setLogLevel(std::min(std::max(log_level(), 0), 4));   // log level must be between 0 and 4
         d_model.setMaximumSeconds(max_seconds());
         d_model.branchAndBound();
