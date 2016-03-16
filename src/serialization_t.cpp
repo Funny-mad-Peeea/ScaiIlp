02f1c8455aa8b85cfb1fbb9b7bbc9363f3958ade
Tobias Brunsch <tobias.brunsch@scai.fraunhofer.de>
2016-03-16 14:41:02 +0100

    Serialization: First draft
    * Too general, too complicated
    * Unnecessary writes

---
+++ src/serialization_t.cpp
@@ -0,0 +1,344 @@
+#include "serialization_t.hpp"
+
+#include "serialization.hpp"
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+const auto c_result_vector_size = 4;
+
+struct Data
+{
+    enum class Enumeration { ENUM1, ENUM2, ENUM3 };
+
+    bool        value_bool;
+    char        value_char;
+    int         value_int;
+    float       value_float;
+    double      value_double;
+    Enumeration value_enum;
+
+    vector<int>                         vector_1_int;
+    vector< vector<double> >            vector_2_double;
+    vector< vector< vector<char> > >    vector_3_char;
+
+};
+
+
+struct Result
+{
+    enum class Enumeration { ENUM1, ENUM2 };
+
+    int value_int;
+    Enumeration value_enum;
+
+    vector<int>                         vector_1_int;
+    vector< vector<double> >            vector_2_double;
+    vector< vector< vector<char> > >    vector_3_char;
+};
+
+
+struct ResultOffset
+{
+    int offset_value_int;
+    int offset_value_enum;
+    int offset_vector_1_int;
+    int offset_vector_2_double;
+    int offset_vector_3_char;
+};
+
+
+template<class T>
+bool operator!=(const vector<T>& p_vector_1, const vector<T>& p_vector_2)
+{
+    if (p_vector_1.size() != p_vector_2.size())
+        return true;
+
+    for (auto i = 0; i < (int) p_vector_1.size(); ++i)
+        if (p_vector_1[i] != p_vector_2[i])
+            return true;
+
+    return false;
+}
+
+
+template<class T>
+bool operator==(const vector<T>& p_vector_1, const vector<T>& p_vector_2)
+{
+    return !(p_vector_1 != p_vector_2);
+}
+
+
+static bool rand_bool()     { return ((rand() % 2) == 0 ? false : true); }
+static char rand_char()     { return (char) (rand() % 256); }
+static int rand_int()       { return rand(); }
+static double rand_double() { return -0.5 + (1.0*rand())/RAND_MAX; }
+static float rand_float()   { return (float) rand_double(); }
+
+static int rand_size()      { return 1 + (rand() % 4); }
+
+static Data generate_random_data()
+{
+    srand(3);
+
+    Data data;
+
+    data.value_bool   = rand_bool();
+    data.value_char   = rand_char();
+    data.value_int    = rand_int();
+    data.value_float  = rand_float();
+    data.value_double = rand_double();
+    data.value_enum = ((rand() % 3) == 0 ? Data::Enumeration::ENUM3 :
+                      ((rand() % 2) == 0 ? Data::Enumeration::ENUM2
+                                         : Data::Enumeration::ENUM1));
+
+    const auto size_1 = rand_size();
+    data.vector_1_int.resize(size_1);
+    for (auto i = 0; i < size_1; ++i)
+        data.vector_1_int[i] = rand_int();
+
+    const auto size_2_1 = rand_size();
+    const auto size_2_2 = rand_size();
+    data.vector_2_double.resize(size_2_1);
+    for (auto i = 0; i < size_2_1; ++i)
+    {
+        data.vector_2_double[i].resize(size_2_2);
+        for (auto j = 0; j < size_2_2; ++j)
+            data.vector_2_double[i][j] = rand_double();
+    }
+
+    const auto size_3_1 = rand_size();
+    const auto size_3_2 = rand_size();
+    const auto size_3_3 = rand_size();
+    data.vector_3_char.resize(size_3_1);
+    for (auto i = 0; i < size_3_1; ++i)
+    {
+        data.vector_3_char[i].resize(size_3_2);
+        for (auto j = 0; j < size_3_2; ++j)
+        {
+            data.vector_3_char[i][j].resize(size_3_3);
+            for (auto k = 0; k < size_3_3; ++k)
+                data.vector_3_char[i][j][k] = rand_char();
+        }
+    }
+
+    return data;
+}
+
+
+static Result generate_random_result()
+{
+    Result result;
+
+    result.value_int = rand_int();
+    result.value_enum = ((rand() % 2) == 0 ? Result::Enumeration::ENUM2
+                                           : Result::Enumeration::ENUM1);
+
+    result.vector_1_int.resize(c_result_vector_size);
+    for (auto i = 0; i < c_result_vector_size; ++i)
+        result.vector_1_int[i] = rand_int();
+
+    result.vector_2_double.resize(c_result_vector_size);
+    for (auto i = 0; i < c_result_vector_size; ++i)
+    {
+        result.vector_2_double[i].resize(c_result_vector_size);
+        for (auto j = 0; j < c_result_vector_size; ++j)
+            result.vector_2_double[i][j] = rand_double();
+    }
+
+    result.vector_3_char.resize(c_result_vector_size);
+    for (auto i = 0; i < c_result_vector_size; ++i)
+    {
+        result.vector_3_char[i].resize(c_result_vector_size);
+        for (auto j = 0; j < c_result_vector_size; ++j)
+        {
+            result.vector_3_char[i][j].resize(c_result_vector_size);
+            for (auto k = 0; k < c_result_vector_size; ++k)
+                result.vector_3_char[i][j][k] = rand_char();
+        }
+    }
+
+    return result;
+}
+
+
+static ResultOffset serialize(const Data& p_data, std::vector<char>* r_memory)
+{
+    Result result;
+
+    result.vector_1_int.resize(c_result_vector_size);
+
+    result.vector_2_double.resize(c_result_vector_size);
+    for (auto& vector: result.vector_2_double)
+        vector.resize(c_result_vector_size);
+
+    result.vector_3_char.resize(c_result_vector_size);
+    for (auto& vectors: result.vector_3_char)
+    {
+        vectors.resize(c_result_vector_size);
+        for (auto& vector: vectors)
+            vector.resize(c_result_vector_size);
+    }
+
+    ResultOffset offset;
+
+    MemoryAligner serializer;
+
+    serializer << p_data.vector_2_double
+               << p_data.value_bool
+               << reserve(result.vector_3_char, offset.offset_vector_3_char)
+               << reserve(result.value_enum, offset.offset_value_enum)
+               << p_data.value_enum
+               << p_data.vector_1_int
+               << p_data.value_int
+               << reserve(result.vector_1_int, offset.offset_vector_1_int)
+               << p_data.value_float
+               << p_data.vector_3_char
+               << reserve(result.vector_2_double, offset.offset_vector_2_double)
+               << p_data.value_double
+               << reserve(result.value_int, offset.offset_value_int)
+               << p_data.value_char;
+
+    r_memory->resize(serializer.required_bytes());
+    serializer.copy_memory(r_memory->data());
+
+    return offset;
+}
+
+
+static Data deserialize(void* p_memory, ResultOffset* v_result_offset)
+{
+    Data data;
+    Result result;
+
+    MemoryAligner deserializer(p_memory);
+
+    deserializer >> data.vector_2_double
+                 >> data.value_bool
+                 >> store_offset(result.vector_3_char, v_result_offset->offset_vector_3_char)
+                 >> store_offset(result.value_enum, v_result_offset->offset_value_enum)
+                 >> data.value_enum
+                 >> data.vector_1_int
+                 >> data.value_int
+                 >> store_offset(result.vector_1_int, v_result_offset->offset_vector_1_int)
+                 >> data.value_float
+                 >> data.vector_3_char
+                 >> store_offset(result.vector_2_double, v_result_offset->offset_vector_2_double)
+                 >> data.value_double
+                 >> store_offset(result.value_int, v_result_offset->offset_value_int)
+                 >> data.value_char;
+
+    return data;
+}
+
+
+static void serialize_result(const Result& p_result, void* v_memory, const ResultOffset& p_offset)
+{
+    MemoryAligner serializer(v_memory);
+    
+    serializer << at_offset(p_result.vector_1_int,    p_offset.offset_vector_1_int)
+               << at_offset(p_result.value_int,       p_offset.offset_value_int)
+               << at_offset(p_result.vector_3_char,   p_offset.offset_vector_3_char)
+               << at_offset(p_result.value_enum,      p_offset.offset_value_enum)
+               << at_offset(p_result.vector_2_double, p_offset.offset_vector_2_double);
+};
+
+
+static Result deserialize_result(void* p_memory, const ResultOffset& p_offset)
+{
+    Result result;
+
+    MemoryAligner deserializer(p_memory);
+
+    deserializer >> at_offset(result.value_enum,      p_offset.offset_value_enum)
+                 >> at_offset(result.value_int,       p_offset.offset_value_int)
+                 >> at_offset(result.vector_1_int,    p_offset.offset_vector_1_int)
+                 >> at_offset(result.vector_2_double, p_offset.offset_vector_2_double)
+                 >> at_offset(result.vector_3_char,   p_offset.offset_vector_3_char);
+
+    return result;
+}
+
+
+static void verify_zero(void* p_memory, const ResultOffset& p_offset)
+{
+    auto result = deserialize_result(p_memory, p_offset);
+
+    assert(result.value_int == 0);
+    assert(static_cast<int>(result.value_enum) == 0);
+    assert(result.vector_1_int.size() == 0);
+    assert(result.vector_2_double.size() == 0);
+    assert(result.vector_3_char.size() == 0);
+}
+
+
+static void verify_equality(const Data& p_data_1, const Data& p_data_2)
+{
+    assert(p_data_1.value_bool      == p_data_2.value_bool);
+    assert(p_data_1.value_char      == p_data_2.value_char);
+    assert(p_data_1.value_int       == p_data_2.value_int);
+    assert(p_data_1.value_float     == p_data_2.value_float);
+    assert(p_data_1.value_double    == p_data_2.value_double);
+    assert(p_data_1.vector_1_int    == p_data_2.vector_1_int);
+    assert(p_data_1.vector_2_double == p_data_2.vector_2_double);
+    assert(p_data_1.vector_3_char   == p_data_2.vector_3_char);
+}
+
+
+static void verify_equality(const ResultOffset& p_offset_1, const ResultOffset& p_offset_2)
+{
+    assert(p_offset_1.offset_value_int       == p_offset_2.offset_value_int);
+    assert(p_offset_1.offset_value_enum      == p_offset_2.offset_value_enum);
+    assert(p_offset_1.offset_vector_1_int    == p_offset_2.offset_vector_1_int);
+    assert(p_offset_1.offset_vector_2_double == p_offset_2.offset_vector_2_double);
+    assert(p_offset_1.offset_vector_3_char   == p_offset_2.offset_vector_3_char);
+}
+
+
+static void verify_equality(const Result& p_result_1, const Result& p_result_2)
+{
+    assert(p_result_1.value_int       == p_result_2.value_int);
+    assert(p_result_1.value_enum      == p_result_2.value_enum);
+    assert(p_result_1.vector_1_int    == p_result_2.vector_1_int);
+    assert(p_result_1.vector_2_double == p_result_2.vector_2_double);
+    assert(p_result_1.vector_3_char   == p_result_2.vector_3_char);
+}
+
+
+void test_serialization()
+{
+    cout << endl
+         << "Serialization test" << endl
+         << "==================" << endl
+         << endl;
+
+    // Alice: Generate data and serialize it
+    const auto data_alice = generate_random_data();
+
+    vector<char> memory;
+    const auto offset_alice = serialize(data_alice, &memory);
+
+    verify_zero(memory.data(), offset_alice);   // result is zero unless Bob serialized his result
+
+    // Bob: Deserialize data and serialize result in the same memory
+    ResultOffset offset_bob;
+    auto data_bob = deserialize(memory.data(), &offset_bob);
+
+    verify_equality(data_alice, data_bob);
+    verify_equality(offset_alice, offset_bob);
+
+    const auto result_bob = generate_random_result();
+    serialize_result(result_bob, memory.data(), offset_bob);
+
+    // Alice: Deserialize result
+    const auto result_alice = deserialize_result(memory.data(), offset_alice);
+
+    verify_equality(result_alice, result_bob);
+
+    cout << "Serialization and deserialization successful." << endl;
+}
--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -1,7 +1,9 @@
 #include "ilp_solver_cbc_t.hpp"
+#include "serialization_t.hpp"
 
 void main()
 {
     ilp_solver::test_sorting_cbc();
     ilp_solver::test_linear_programming_cbc();
+    test_serialization();
 }
