0ea4b253185fe6bf64027c220987520e42b1eaa0
Onno Garms <onno.garms@scai.fraunhofer.de>
2016-05-03 17:02:44 +0200

    Kommentare zu SolverExitCode

--- src/production/solver_exit_code.hpp
+++ src/production/solver_exit_code.hpp
@@ -5,10 +5,12 @@ enum SolverExitCode
 {
     ok = 0,
 
-    uncaught_exception_1 = 3,
-    uncaught_exception_2 = -529697949,
+    // Exit codes that we observed when CBC crashed
+    uncaught_exception_1 = 3,          // when an exception, namely "Out Of Memory", is thrown from a thread in CBC but not caught
+    uncaught_exception_2 = -529697949, // seems to indicate "Out Of Memory (C++ Exception)", see http://www.primegrid.com/forum_thread.php?id=693 or enter this number in Google
 
-    out_of_memory        = 14142,   // arbitrary value that is unlikely to be used by Windows for internal codes
+    // Our own exit codes, starting from an arbitrary value that is unlikely to be used by Windows for internal codes
+    out_of_memory        = 14142,
     command_line_error,
     shared_memory_error,
     model_error,
