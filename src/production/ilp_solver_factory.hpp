b302123d5b322fbed9ddd52859081a040907d285
Nick Röder <nick.roeder@scai.fraunhofer.de>
2018-10-05 11:47:02 +0200

    AXS-2175: ScaiILP Properties zu nutzenden Projekten hinzugefügt (vor global_properties), deprecated-attribute eingeführt.

--- src/production/ilp_solver_factory.hpp
+++ src/production/ilp_solver_factory.hpp
@@ -3,12 +3,21 @@
 
 #include "ilp_solver_interface.hpp"
 
+
+#if (WITH_CBC == 1)
+#define MAY_USE_CBC
+#else
+#define MAY_USE_CBC [[deprecated("CBC is deactivated. You can not create a CBC Solver.")]]
+#endif
+
 namespace ilp_solver
 {
-    extern "C" ILPSolverInterface* __stdcall create_solver_cbc();
-    extern "C" ILPSolverInterface* __stdcall create_solver_stub(const char* p_executable_basename);
+    extern "C" MAY_USE_CBC ILPSolverInterface* __stdcall create_solver_cbc();
+    extern "C"             ILPSolverInterface* __stdcall create_solver_stub(const char* p_executable_basename);
 
     extern "C" void __stdcall destroy_solver(ILPSolverInterface* p_solver);
 }
+
+#undef MAY_USE_CBC
 
 #endif
